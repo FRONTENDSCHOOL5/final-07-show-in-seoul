02bec4f86d7dd7d73ba60fa9fef4080e1c61a571
sewon-O <tpdnjs7878@naver.com>
2023-06-13 21:34:28 +0900

    [Fix] 프로필 함수 rsc로 수정, 스타일 컴포넌트 위치를 하단으로 이동, SASS 문법 적용

--- src/Components/Common/Profile.jsx
+++ src/Components/Common/Profile.jsx
@@ -1,20 +1,68 @@
 import React from 'react';
-import GlobalStyle from '../../Styles/GlobalStyle';
 import styled, { css } from 'styled-components';
+
+const Profile = () => {
+  return (
+    <>
+      {/* 프로필 컨테이너 */}
+      <ProfileSection>
+        {/* 프로필 이미지,이름,아이디 */}
+        <img src="./src/assets/logo.png"></img>
+        <ProfileName>찰리 채플린의 초콜릿 공장</ProfileName>
+        <ProfileId>@ char_chocolate</ProfileId>
+        {/* 프로필 관심 리스트 표시 */}
+        <ul>
+          <li>연극</li>
+          <li>뮤지컬</li>
+          <li>피아노 연주회</li>
+        </ul>
+        {/* 프로필 수정 버튼 */}
+        <button>프로필 수정</button>
+      </ProfileSection>
+    </>
+  );
+};
+
+export default Profile;
 
 // 프로필 컨테이너
 const ProfileSection = styled.section`
   width: 390px;
   text-align: center;
   border-bottom: 1px solid #dbdbdb;
-`;
-
-// 프로필 이미지
-const ProfileImg = styled.img`
-  width: 90px;
-  height: 90px;
-  border-radius: 50%;
-  margin: 30px auto 0;
+  // 프로필 이미지
+  img {
+    width: 90px;
+    height: 90px;
+    border-radius: 50%;
+    margin: 30px auto 0;
+  }
+  // 프로필 관심 목록
+  ul {
+    display: flex;
+    gap: 5px;
+    justify-content: center;
+    margin-top: 10px;
+    li {
+      font-size: 12px;
+      border-radius: 20px;
+      background-color: #961f1f;
+      color: #ffffff;
+      padding: 4px 10px;
+    }
+  }
+  // 프로필 수정 버튼
+  button {
+    border: none;
+    font-size: 14px;
+    color: #767676;
+    border: 1px solid #dbdbdb;
+    border-radius: 20px;
+    padding: 6px 26px;
+    margin: 16px 0;
+    background-color: #ffffff;
+    cursor: pointer;
+  }
 `;
 
 // 프로필 이름
@@ -31,55 +79,3 @@ const ProfileId = styled.p`
   color: rgba(118, 118, 118, 1);
   margin-top: 6px;
 `;
-
-// 프로필 관심 목록
-const ProfileInterest = styled.ul`
-  display: flex;
-  gap: 5px;
-  justify-content: center;
-  margin-top: 10px;
-`;
-const ProfileInterestLi = styled.li`
-  font-size: 12px;
-  border-radius: 20px;
-  background-color: #961f1f;
-  color: #ffffff;
-  padding: 4px 10px;
-`;
-
-// 프로필 수정 버튼
-const ProfileBtn = styled.button`
-  border: none;
-  font-size: 14px;
-  color: #767676;
-  border: 1px solid #dbdbdb;
-  border-radius: 20px;
-  padding: 6px 26px;
-  margin: 16px 0;
-  background-color: #ffffff;
-  cursor: pointer;
-`;
-
-const Profile = () => {
-  return (
-    <>
-      {/* 프로필 컨테이너 */}
-      <ProfileSection>
-        {/* 프로필 이미지,이름,아이디 */}
-        <ProfileImg src="./src/assets/logo.png"></ProfileImg>
-        <ProfileName>찰리 채플린의 초콜릿 공장</ProfileName>
-        <ProfileId>@ char_chocolate</ProfileId>
-        {/* 프로필 관심 리스트 표시 */}
-        <ProfileInterest>
-          <ProfileInterestLi>연극</ProfileInterestLi>
-          <ProfileInterestLi>뮤지컬</ProfileInterestLi>
-          <ProfileInterestLi>피아노 연주회</ProfileInterestLi>
-        </ProfileInterest>
-        {/* 프로필 수정 버튼 */}
-        <ProfileBtn>프로필 수정</ProfileBtn>
-      </ProfileSection>
-    </>
-  );
-};
-
-export default Profile;
