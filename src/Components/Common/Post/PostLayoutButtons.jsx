b608a8451a78aa25ce8e30d03d2ff4a94243a88c
sewon-O <tpdnjs7878@naver.com>
2023-06-15 10:10:44 +0900

    [Chore] 프로필 레이아웃 버튼 이미지 적용

--- src/Components/Common/Post/PostLayoutButtons.jsx
+++ src/Components/Common/Post/PostLayoutButtons.jsx
@@ -1,5 +1,9 @@
 import React from 'react';
-import styled, { css } from 'styled-components';
+import styled from 'styled-components';
+import ListOff from '../../../Assets/Icon/icon-post-list-off.png';
+import ListOn from '../../../Assets/Icon/icon-post-list-on.png';
+import AlbumOff from '../../../Assets/Icon/icon-post-album-off.png';
+import AlbumOn from '../../../Assets/Icon/icon-post-album-on.png';
 
 const PostLayoutButtons = () => {
   return (
@@ -30,18 +34,18 @@ const LayoutButtons = styled.div`
 
 const ListButton = styled.button`
   border-right: none;
-  background: url('./src/assets/icon-post-list-off.png') no-repeat center;
+  background: url(${ListOff}) no-repeat center;
   &:hover {
     border-bottom-color: #767676;
-    background-image: url('./src/assets/icon-post-list-on.png');
+    background-image: url(${ListOn});
   }
 `;
 
 const GridButton = styled.button`
   border-left: none;
-  background: url('./src/assets/icon-post-list-off.png') no-repeat center;
+  background: url(${AlbumOff}) no-repeat center;
   &:hover {
     border-bottom-color: #767676;
-    background-image: url('./src/assets/icon-album-list-on.png');
+    background-image: url(${AlbumOn});
   }
 `;
