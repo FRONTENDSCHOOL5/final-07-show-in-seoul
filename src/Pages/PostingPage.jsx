b8545aa231b24c8d6659253b9c47976c58a54fb3
sewon-O <tpdnjs7878@naver.com>
2023-06-15 17:41:07 +0900

    [Feat] 게시글 작성 페이지

--- src/Pages/PostingPage.jsx
+++ src/Pages/PostingPage.jsx
@@ -1,7 +1,20 @@
 import React from 'react';
+import TopBar from '../Components/Common/TopBar';
 
 const PostingPage = () => {
-  return <div></div>;
+  return (
+    <>
+      <TopBar leftEl="back" rightEl="upload" />
+      <textarea required rows="10" cols="33" placeholder="게시글 입력하기.."></textarea>
+      <div>
+        <img></img>
+      </div>
+      <label>
+        <img />
+        <input type="file" style={{ display: 'none' }} />
+      </label>
+    </>
+  );
 };
 
 export default PostingPage;
