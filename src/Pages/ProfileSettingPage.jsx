c58288ebadbdeaec3f12c2e60f2908f01d2827a1
d-charlie-kim <d.charlie.kim@gmail.com>
2023-06-15 17:20:48 +0900

    [Feat] 프로필 설정 페이지 마크업 초안

--- src/Pages/ProfileSettingPage.jsx
+++ src/Pages/ProfileSettingPage.jsx
@@ -3,9 +3,14 @@ import { styled } from 'styled-components';
 import Button from '../Components/Common/Button';
 import InputBox from '../Components/Common/InputBox';
 import InterestsTag from '../Components/Profile/InterestsTag';
-import { Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
 const ProfileSettingPage = () => {
+  const navigate = useNavigate();
+
+  function movetomain() {
+    navigate('/mainpage');
+  }
   return (
     <SProfileSetting>
       <div className="profile-setting-header">
@@ -17,12 +22,14 @@ const ProfileSettingPage = () => {
       <div className="profile-setting-info">
         <InputBox title="사용자 이름" placeholder="2~10자 이내여야 합니다." />
         <InputBox title="계정 ID" placeholder="영문, 숫자, 특수문자(.),(_)만 사용 가능합니다." />
-        <InputBox title="취향dd" disabled={true} placeholder="최대 4개까지 선택할 수 있습니다." />
+        <InputBox title="취향" disabled={true} placeholder="최대 4개까지 선택할 수 있습니다." />
         <InterestsTag etc={false} />
       </div>
-      <Link to="https://www.naver.com">눌러보세요~</Link>
-
-      <Button size="Large">시작하기</Button>
+      <div onClick={movetomain}>
+        <Button onClick={movetomain} size="Large">
+          시작하기
+        </Button>
+      </div>
     </SProfileSetting>
   );
 };
@@ -30,6 +37,9 @@ const ProfileSettingPage = () => {
 export default ProfileSettingPage;
 
 const SProfileSetting = styled.div`
+  width: 100%;
+  padding: 0 22px;
+
   .profile-setting-header {
     padding: 30px 0 40px 0;
     display: flex;
@@ -44,11 +54,13 @@ const SProfileSetting = styled.div`
       font-size: 14px;
     }
   }
+
   .profile-setting-info {
     display: flex;
     flex-direction: column;
     align-items: center;
     height: 325px;
-    margin-bottom: 150px;
+    margin-bottom: 50px;
+    gap: 15px;
   }
 `;
