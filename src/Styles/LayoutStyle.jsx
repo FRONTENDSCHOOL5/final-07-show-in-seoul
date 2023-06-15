fc8e43d5905a2b267668f62615711b25e3bbcf0d
d-charlie-kim <d.charlie.kim@gmail.com>
2023-06-15 10:32:50 +0900

    [Style] 글로벌 스타일 추가, 레이아웃 추가

---
+++ src/Styles/LayoutStyle.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { styled } from 'styled-components';
+
+const LayoutStyle = () => {
+  return <SLayoutStyle></SLayoutStyle>;
+};
+
+export default LayoutStyle;
+
+const SLayoutStyle = styled.div`
+  margin: 0 auto;
+  width: 390px;
+  height: 829px;
+  background-color: white;
+`;
