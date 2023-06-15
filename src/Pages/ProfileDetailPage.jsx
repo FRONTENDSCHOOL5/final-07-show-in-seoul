3bfbb083ca2874b9f7c705a6bbcd0584156f963b
sewon-O <tpdnjs7878@naver.com>
2023-06-15 13:30:56 +0900

    [Feat] 프로필 디테일 페이지

--- src/Pages/ProfileDetailPage.jsx
+++ src/Pages/ProfileDetailPage.jsx
@@ -1,7 +1,20 @@
 import React from 'react';
+import TopBar from '../Components/Common/TopBar';
+import Profile from '../Components/Common/Profile';
+import PostLayoutButtons from '../Components/Common/Post/PostLayoutButtons';
+import Post from '../Components/Common/Post/Post';
+import BottomNav from '../Components/Common/BottomNav';
 
 const ProfileDetailPage = () => {
-  return <div></div>;
+  return (
+    <>
+      <TopBar leftEl="back" rightEl="logout" />
+      <Profile />
+      <PostLayoutButtons />
+      <Post />
+      <BottomNav />
+    </>
+  );
 };
 
 export default ProfileDetailPage;
