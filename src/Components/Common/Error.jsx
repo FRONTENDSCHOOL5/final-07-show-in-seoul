2a0025fb188ccac70a7d07cfd9f7e79fc97fb83b
kimsojin <ksd9546@naver.com>
2023-06-17 21:28:11 +0900

    [Feat] 에러 페이지 공통 컴포넌트
    - 3개의 에러페이지에 공통으로 들어가는 부분을 공통 컴포넌트화 했습니다.
    - 에러페이지에 공통으로 들어가는 이미지가 있고, 각 페이지에 맞는 에러 메세지 작성을 위해 text프롭스를 받고 있습니다.

---
+++ src/Components/Common/Error.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import styled from 'styled-components';
+import error from '../../Assets/Img/error.svg';
+
+const Error = ({ text }) => {
+  return (
+    <>
+      <SError>
+        {/* 에러 페이지 기본 이미지입니다*/}
+        <img src={error} alt="에러화면 이미지입니다" className="error-img" />
+        {/* 페이지에 맞는 에러 text를 작성해주세요*/}
+        <p className="error-text">{text}</p>
+      </SError>
+    </>
+  );
+};
+
+export default Error;
+
+const SError = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  gap: 20px;
+
+  .error-text {
+    color: #767676;
+    font-size: 14px;
+  }
+`;
