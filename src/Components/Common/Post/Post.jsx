8610b0241009f9c4f5db7bdc8f381634147f85e9
sewon-O <tpdnjs7878@naver.com>
2023-06-17 20:50:31 +0900

    [Style] 게시글 페이지에서 탑바와 바텀네비바 고정시키고 그 안에 내용물들이 많아지는 경우를 위한 코드 추가

--- src/Components/Common/Post/Post.jsx
+++ src/Components/Common/Post/Post.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import styled, { css } from 'styled-components';
+import styled from 'styled-components';
 import PostHeader from './PostHeader';
 import PostContentButtons from './PostContentButtons';
 import PostContent from './PostContent';
--- src/Pages/PostPage.jsx
+++ src/Pages/PostPage.jsx
@@ -2,17 +2,31 @@ import React from 'react';
 import TopBar from '../Components/Common/TopBar';
 import Post from '../Components/Common/Post/Post';
 import BottomNav from '../Components/Common/BottomNav';
+import styled from 'styled-components';
 
 const PostPage = () => {
   return (
     <>
       <TopBar rightEl="newPost" />
-      <ul>
+      <SUl>
         <Post />
-      </ul>
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+      </SUl>
       <BottomNav />
     </>
   );
 };
 
 export default PostPage;
+
+const SUl = styled.ul`
+  height: calc(100% - 108px);
+  overflow-y: scroll;
+  padding-top: 4px;
+  &::-webkit-scrollbar {
+    display: none;
+  }
+`;
