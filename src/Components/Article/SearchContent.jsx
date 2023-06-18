5f16f752072efccd2eb2b647d82c5d0ab0e2bbcd
zxxng <jeayeong123@naver.com>
2023-06-19 00:44:33 +0900

    [Feat] 검색페이지 마크업 완료

--- src/Components/Article/SearchContent.jsx
+++ src/Components/Article/SearchContent.jsx
@@ -1,16 +1,17 @@
 import React from 'react';
 import styled from 'styled-components';
 import { Link } from 'react-router-dom';
-import imgTest from '../../Assets/Img/포스터.jpeg';
 
-const SearchContent = () => {
+const SearchContent = ({ data, keyword }) => {
   return (
-    <SLink>
-      <img src={imgTest} alt="포스터" />
+    <SLink to="/ShowDetailPage">
+      <img src={data.main_img} alt="포스터" />
       <div class="container">
-        <h3>공연 제목 공연 제목 공연 제목 공연 제목 공연 제목 공연 제목</h3>
-        <p>공연 장소 / 지역구</p>
-        <p>공연 일정</p>
+        <h2>{data.title}</h2>
+        <p>
+          {data.place} / {data.guname}
+        </p>
+        <p>{data.date}</p>
       </div>
     </SLink>
   );
@@ -31,7 +32,7 @@ const SLink = styled(Link)`
   }
   .container {
     width: 260px;
-    h3 {
+    h2 {
       display: inline-block;
       font-size: 14px;
       font-weight: bold;
@@ -39,10 +40,7 @@ const SLink = styled(Link)`
       /* 여러줄 다 보이기 */
       overflow: hidden;
       white-space: normal;
-      /* 스타일 추가 */
-      text-align: left;
       line-height: 1.2;
-      word-wrap: break-word;
       display: -webkit-box;
       -webkit-line-clamp: 2;
       -webkit-box-orient: vertical;
