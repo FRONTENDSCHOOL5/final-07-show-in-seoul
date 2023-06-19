41bf1b3a9d4c23622eab9d7c00e630994a701cd0
zxxng <jeayeong123@naver.com>
2023-06-19 22:06:00 +0900

    [Feat] 하단 네비게이션 클릭시 페이지 이동 기능 추가

--- src/Components/Common/BottomNav.jsx
+++ src/Components/Common/BottomNav.jsx
@@ -10,19 +10,23 @@ import iconSearchFill from '../../Assets/Icon/icon-search-fill.svg';
 import iconHomeFill from '../../Assets/Icon/icon-home-fill.svg';
 import iconPostFill from '../../Assets/Icon/icon-post-fill.svg';
 import iconUserFill from '../../Assets/Icon/icon-user-fill.svg';
+import { Link } from 'react-router-dom';
+
+// import { useRecoilState } from 'recoil';
+// import { bottomNavIndex } from '../../Atom/atom';
 
 const BottomNav = props => {
-  console.log(props);
   // 현재 선택된 a 태그 id 확인
   const [activeIcon, setActiveIcon] = useState(3);
+  // const [bottomNavIndexAtom, setbottomNavIndex] = useRecoilState(bottomNavIndex);
 
   // navigation 아이콘 데이터
   const iconList = [
-    { id: 1, title: '카테고리', off: iconCategory, on: iconCategoryFill },
-    { id: 2, title: '검색', off: iconSearch, on: iconSearchFill },
-    { id: 3, title: '홈', off: iconHome, on: iconHomeFill },
-    { id: 4, title: '게시판', off: iconPost, on: iconPostFill },
-    { id: 5, title: '프로필', off: iconUser, on: iconUserFill },
+    { id: 1, title: '카테고리', off: iconCategory, on: iconCategoryFill, link: '/categorypage' },
+    { id: 2, title: '검색', off: iconSearch, on: iconSearchFill, link: '/searchpage' },
+    { id: 3, title: '홈', off: iconHome, on: iconHomeFill, link: '/mainpage' },
+    { id: 4, title: '게시판', off: iconPost, on: iconPostFill, link: '/postpage' },
+    { id: 5, title: '프로필', off: iconUser, on: iconUserFill, link: '/profiledetailpage' },
   ];
 
   // 클릭 시 해당 아이콘 id로 activeIcon 변경되며 재렌더링
@@ -36,11 +40,11 @@ const BottomNav = props => {
         {iconList.map(icon => {
           return (
             <li key={icon.id}>
-              <a href="#" onClick={() => handleClick(icon.id)}>
+              <Link to={icon.link} onClick={() => handleClick(icon.id)}>
                 {/* 현재 activeIcon 번호와 id 가 일치하면 on 아이콘 표시 & p 색상 변경 */}
                 <img src={activeIcon === icon.id ? icon.on : icon.off} alt={icon.title} />
                 <Title color={activeIcon === icon.id ? '#961F1F' : '#767676'}>{icon.title}</Title>
-              </a>
+              </Link>
             </li>
           );
         })}
@@ -61,13 +65,10 @@ const NavList = styled.ul`
   border-top: solid 1px var(--gray);
   position: fixed;
   bottom: 0;
-  a {
-    text-decoration: none;
-    img {
-      width: 24px;
-      height: 24px;
-      margin-bottom: 4px;
-    }
+  img {
+    width: 24px;
+    height: 24px;
+    margin-bottom: 4px;
   }
 `;
 
