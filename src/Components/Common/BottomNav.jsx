b9d8f4ca2831d6c1109b7c9b66e2438c48b926d6
zxxng <jeayeong123@naver.com>
2023-06-20 00:06:34 +0900

    [Feat] 페이지 이동시 아이콘 상태 변경 기능 추가

--- src/Components/Common/BottomNav.jsx
+++ src/Components/Common/BottomNav.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import styled from 'styled-components';
 import iconCategory from '../../Assets/Icon/icon-category.svg';
 import iconSearch from '../../Assets/Icon/icon-search.svg';
@@ -10,29 +10,28 @@ import iconSearchFill from '../../Assets/Icon/icon-search-fill.svg';
 import iconHomeFill from '../../Assets/Icon/icon-home-fill.svg';
 import iconPostFill from '../../Assets/Icon/icon-post-fill.svg';
 import iconUserFill from '../../Assets/Icon/icon-user-fill.svg';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 
-// import { useRecoilState } from 'recoil';
-// import { bottomNavIndex } from '../../Atom/atom';
-
-const BottomNav = props => {
-  // 현재 선택된 a 태그 id 확인
-  const [activeIcon, setActiveIcon] = useState(3);
-  // const [bottomNavIndexAtom, setbottomNavIndex] = useRecoilState(bottomNavIndex);
-
+const BottomNav = () => {
   // navigation 아이콘 데이터
   const iconList = [
-    { id: 1, title: '카테고리', off: iconCategory, on: iconCategoryFill, link: '/categorypage' },
-    { id: 2, title: '검색', off: iconSearch, on: iconSearchFill, link: '/searchpage' },
-    { id: 3, title: '홈', off: iconHome, on: iconHomeFill, link: '/mainpage' },
-    { id: 4, title: '게시판', off: iconPost, on: iconPostFill, link: '/postpage' },
-    { id: 5, title: '프로필', off: iconUser, on: iconUserFill, link: '/profiledetailpage' },
+    { id: 1, title: '카테고리', off: iconCategory, on: iconCategoryFill, link: ['/categorypage'] },
+    { id: 2, title: '검색', off: iconSearch, on: iconSearchFill, link: ['/searchpage'] },
+    { id: 3, title: '홈', off: iconHome, on: iconHomeFill, link: ['/mainpage'] },
+    { id: 4, title: '게시판', off: iconPost, on: iconPostFill, link: ['/postpage'] },
+    { id: 5, title: '프로필', off: iconUser, on: iconUserFill, link: ['/profiledetailpage'] },
   ];
 
-  // 클릭 시 해당 아이콘 id로 activeIcon 변경되며 재렌더링
-  const handleClick = id => {
-    setActiveIcon(id);
-  };
+  // 현재 페이지 경로 확인
+  const location = useLocation();
+  const path = location.pathname;
+  // 현재 페이지 경로가 포함되는 id 추출, 없으면 기본값 3(home)
+  let activeIcon = iconList.find(item => item.link.includes(path));
+  if (activeIcon !== undefined) {
+    activeIcon = activeIcon.id;
+  } else {
+    activeIcon = 3;
+  }
 
   return (
     <>
@@ -40,7 +39,7 @@ const BottomNav = props => {
         {iconList.map(icon => {
           return (
             <li key={icon.id}>
-              <Link to={icon.link} onClick={() => handleClick(icon.id)}>
+              <Link to={icon.link[0]}>
                 {/* 현재 activeIcon 번호와 id 가 일치하면 on 아이콘 표시 & p 색상 변경 */}
                 <img src={activeIcon === icon.id ? icon.on : icon.off} alt={icon.title} />
                 <Title color={activeIcon === icon.id ? '#961F1F' : '#767676'}>{icon.title}</Title>
