f9cf2aa6af6b788425924b345014345d027061f4
sewon-O <tpdnjs7878@naver.com>
2023-06-24 05:17:38 +0900

    [Feat] 게시글 헤더 눌렀을 경우 프로필 디테일 페이지로 스테이트 전달

--- src/Components/Common/Post/PostContent.jsx
+++ src/Components/Common/Post/PostContent.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import styled, { css } from 'styled-components';
 
 const PostContent = ({ postsData }) => {
+  console.log(postsData.content);
   return (
     <>
       <SPostContentA>
--- src/Components/Common/Post/PostHeader.jsx
+++ src/Components/Common/Post/PostHeader.jsx
@@ -1,19 +1,68 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
+import { useRecoilValue } from 'recoil';
+import { Link, useNavigate } from 'react-router-dom';
+
+// 이미지
 import iconSmallMore from '../../../Assets/Icon/s-icon-more-vertical.svg';
 import basicProfileImg from '../../../Assets/Img/basic-profile-img.svg';
 
+// recoil
+import { Token } from '../../../Atom/atom';
+
 const PostHeader = ({ postsData }) => {
+  // const getMyToken = useRecoilValue(Token);
+  // const URL = 'https://api.mandarin.weniv.co.kr';
+  // const postsData = { postsData };
+
+  // const GetOtherProfileAPI = ({ postsData }) => {
+  //   const accountName = postsData.author.accountname;
+  //   const [otherProfile, setOtherProfile] = useState([]);
+  //   const getOtherProfile = async ({}) => {
+  //     try {
+  //       const req = {
+  //         method: 'GET',
+  //         headers: {
+  //           'Content-Type': 'application/json',
+  //           Authorization: 'Bearer ' + getMyToken,
+  //         },
+  //       };
+
+  //       const response = await fetch(URL + '/profile/:' + { accountName }, req);
+  //       const data = await response.json();
+  //       setOtherProfile(data);
+  //       if (!response.ok) throw new Error('다른 프로필 보기 에러');
+  //     } catch (e) {
+  //       console.error(e);
+  //     }
+  //   };
+  //   useEffect(() => {
+  //     getOtherProfile();
+  //   }, []);
+  //   return otherProfile.profile.image;
+  // };
+
+  // const otherProfileImg = GetOtherProfileAPI({ postsData });
+  console.log(postsData);
+  const navigate = useNavigate();
+  const test = () => {
+    navigate('/profiledetailpage', { state: postsData });
+  };
+
+  const accountname = postsData.author.accountname;
+  const username = postsData.author.username;
+  console.log(accountname);
+  console.log(username);
   return (
     <>
       <SPostHeaderDiv>
-        <a>
+        <a onClick={test}>
           {/* 프로필 이미지 데이터가 없다면 기본 이미지로 대체*/}
           {/* {postsData.image ? <img src={프로필 설정페이지에서 받아오는 이미지} alt="" /> : <img src={basicProfileImg} alt="" />} */}
           <img src={basicProfileImg} alt="" />
           <div>
-            <SPostUserName>{postsData.author.username}</SPostUserName>
-            <SPostUserId>{postsData.author.accountname}</SPostUserId>
+            <SPostUserName>{username}</SPostUserName>
+            <SPostUserId>{accountname}</SPostUserId>
           </div>
         </a>
         <button></button>
