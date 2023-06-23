63ae88e340164d7dcabddcafcfeddf6e4d0235f2
sewon-O <tpdnjs7878@naver.com>
2023-06-24 05:17:00 +0900

    [Feat] 게시글 헤더 눌렀을 경우 프로필 디테일 페이지로 스테이트 전달

--- src/Pages/ProfileDetailPage.jsx
+++ src/Pages/ProfileDetailPage.jsx
@@ -1,12 +1,24 @@
 import React from 'react';
 import styled from 'styled-components';
+import { useLocation } from 'react-router-dom';
+
+// 공통 컴포넌트
 import TopBar from '../Components/Common/TopBar';
 import Profile from '../Components/Common/Profile';
 import PostLayoutButtons from '../Components/Common/Post/PostLayoutButtons';
 import Post from '../Components/Common/Post/Post';
 import BottomNav from '../Components/Common/BottomNav';
 
+// recoil
+import { MyAccountName } from '../Atom/atom';
+import { useRecoilValue } from 'recoil';
+
 const ProfileDetailPage = () => {
+  const getMyAcoountName = useRecoilValue(MyAccountName);
+  console.log(getMyAcoountName);
+  const accountname = useLocation().state.author.accountname;
+  console.log(accountname);
+
   return (
     <>
       <TopBar leftEl="back" rightEl="logout" />
