679fba74c76bee8352d8a3b1b009a3897a73e4ed
sewon-O <tpdnjs7878@naver.com>
2023-06-26 15:39:20 +0900

    [Feat] 게시글 피드 페이지 모달 임시로 연결

--- src/Components/Common/Post/PostHeader.jsx
+++ src/Components/Common/Post/PostHeader.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
 import { Link, useNavigate } from 'react-router-dom';
 import { useRecoilValue } from 'recoil';
@@ -10,6 +10,10 @@ import basicProfileImg from '../../../Assets/Img/basic-profile-img.svg';
 // recoil
 import { MyAccountName } from '../../../Atom/atom';
 
+//
+import Modal from '../../Modal/Modal';
+import AlertModal from '../../Modal/Alert';
+
 const PostHeader = ({ postsData }) => {
   // console.log(postsData);
   const getMyAccounName = useRecoilValue(MyAccountName);
@@ -19,6 +23,43 @@ const PostHeader = ({ postsData }) => {
   const navigate = useNavigate();
   const goToPostEdit = () => {
     navigate('/posteditpage', { state: postsData });
+  };
+
+  // 모달 연결
+  const [isModalVisible, setIsModalVisible] = useState(false);
+  const [isSModalVisible, setIsSModalVisible] = useState(false);
+  const [isOtherSModalVisible, setIsOtherSModalVisible] = useState(false);
+  const [isDeleteModalVisible, setIsDeleteModalVisible] = useState(false);
+  const [isReporModalVisible, setIsReporModalVisible] = useState(false);
+
+  // alert 모달 닫아주는
+  const closeModal = () => {
+    setIsModalVisible(false);
+    setIsDeleteModalVisible(false);
+    setIsReporModalVisible(false);
+  };
+  // 게시글이 내 거일때 열어주는 모달
+  const openSModal = () => {
+    setIsSModalVisible(true);
+  };
+  // 게시글이 다른 유저일 때 열어주는 모달
+  const openSOtherModal = () => {
+    setIsOtherSModalVisible(true);
+  };
+  // 수정
+  const handleEdit = () => {
+    setIsSModalVisible(false);
+    setIsModalVisible(true);
+  };
+  // 삭제
+  const handeleDelete = () => {
+    setIsSModalVisible(false);
+    setIsDeleteModalVisible(true);
+  };
+  // 신고
+  const handleReport = () => {
+    setIsOtherSModalVisible(false);
+    setIsReporModalVisible(true);
   };
 
   return (
@@ -32,7 +73,38 @@ const PostHeader = ({ postsData }) => {
             <SPostUserId>{accountname}</SPostUserId>
           </div>
         </Link>
-        {accountname === getMyAccounName ? <button onClick={goToPostEdit}></button> : <button></button>}
+        {/* {accountname === getMyAccounName ? <button onClick={goToPostEdit}></button> : <button></button>} */}
+        {accountname === getMyAccounName ? (
+          <button className="postSet" onClick={openSModal}></button>
+        ) : (
+          <button className="postSet" onClick={openSOtherModal}></button>
+        )}
+        {isSModalVisible && (
+          <Modal>
+            <p onClick={handeleDelete}>삭제</p>
+            <p onClick={handleEdit}>수정</p>
+          </Modal>
+        )}
+        {isOtherSModalVisible && (
+          <Modal>
+            <p onClick={handleReport}>신고</p>
+          </Modal>
+        )}
+        {isModalVisible && (
+          <AlertModal confirmText="수정" onConfirm={goToPostEdit} onCancel={closeModal}>
+            게시글을 수정할까요?
+          </AlertModal>
+        )}
+        {isDeleteModalVisible && (
+          <AlertModal confirmText="삭제" onConfirm={closeModal} onCancel={closeModal}>
+            게시글을 삭제할까요?
+          </AlertModal>
+        )}
+        {isReporModalVisible && (
+          <AlertModal confirmText="신고" onConfirm={closeModal} onCancel={closeModal}>
+            게시글을 신고할까요?
+          </AlertModal>
+        )}
       </SPostHeaderDiv>
     </>
   );
@@ -60,7 +132,7 @@ const SPostHeaderDiv = styled.div`
     }
   }
   // 게시물 수정/삭제 모달 띄워주는 버튼
-  button {
+  .postSet {
     border: none;
     width: 18px;
     height: 18px;
