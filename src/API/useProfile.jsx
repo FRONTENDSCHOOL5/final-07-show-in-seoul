ee4d506b33bdfb060cfc6b43bab9d93321e497bc
d-charlie-kim <d.charlie.kim@gmail.com>
2023-06-27 05:38:55 +0900

    [Feat] 프로필 가져오기, 수정하기 훅으로 전환

---
+++ src/API/useProfile.jsx
@@ -0,0 +1,51 @@
+import { useRecoilValue } from 'recoil';
+import { Token } from '../Atom/atom';
+const URL = 'https://api.mandarin.weniv.co.kr';
+
+const useProfile = () => {
+  const getMyToken = useRecoilValue(Token);
+
+  const GetMyProfileAPI = async () => {
+    try {
+      const req = {
+        method: 'GET',
+        headers: {
+          Authorization: 'Bearer ' + getMyToken,
+        },
+      };
+
+      const response = await fetch(URL + '/user/myinfo', req);
+
+      if (!response.ok) throw new Error('프로필 정보 불러오기 에러');
+
+      return await response.json();
+    } catch (e) {
+      console.error(e);
+    }
+  };
+
+  const EditProfileAPI = async profile => {
+    try {
+      const req = {
+        method: 'PUT',
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: 'Bearer ' + getMyToken,
+        },
+        body: JSON.stringify({ ...profile }),
+      };
+
+      const response = await fetch(URL + '/user', req);
+
+      if (!response.ok) throw new Error('프로필 수정 에러');
+
+      return await response.json();
+    } catch (e) {
+      console.error(e);
+    }
+  };
+
+  return { GetMyProfileAPI, EditProfileAPI };
+};
+
+export default useProfile;
