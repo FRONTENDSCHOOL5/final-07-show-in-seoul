c4d08634ce012cf61d3e52fea61fe01c0efe6145
sewon-O <tpdnjs7878@naver.com>
2023-06-28 10:06:07 +0900

    [Chore] 게시글 이미지 박스 쉐도우 적용

--- src/Components/Common/Post/PostContent.jsx
+++ src/Components/Common/Post/PostContent.jsx
@@ -118,6 +118,7 @@ const SPostContentA = styled.a`
       width: 160px;
       object-fit: cover;
       border-radius: 20px;
+      box-shadow: rgba(50, 50, 93, 0.25) 0px 2px 5px -1px, rgba(0, 0, 0, 0.3) 0px 1px 3px -1px;
     }
   }
 `;
